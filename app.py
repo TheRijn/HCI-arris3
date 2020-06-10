3a9ca94eb796769f3f58a6c99301284dfc8ca50b
Marijn Jansen <marijn@jansen.onl>
2020-06-10 21:05:16 +0200

    Login System

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_login import LoginManager, login_required, logout_user, current_user, login_user
+from flask_migrate import Migrate
+
+from forms import LoginForm, RegistrationForm
+from models import db, User
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "this-is-really-secret"
+
+# Setup db and migrations
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Flask-admin
+admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('home'))
+
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run()
---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    __password_hash = db.Column(db.String(128))
+
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
+    birthday = db.Column(db.Date)
+
+    def __init__(self, email):
+        self.email = email
+
+    # https://dev.to/kaelscion/authentication-hashing-in-sqlalchemy-1bem
+    def set_password(self, password):
+        self.__password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.__password_hash, password)
