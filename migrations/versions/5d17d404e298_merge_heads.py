82bca9674a5c0e1fd5fbfc8b4a62dea3712fa732
Marijn Jansen <marijn@jansen.onl>
2020-06-16 14:13:22 +0200

    merge db

---
+++ migrations/versions/5d17d404e298_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 5d17d404e298
+Revises: 5d8d48f2c1ed, 047f07692963
+Create Date: 2020-06-16 14:05:38.986875
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5d17d404e298'
+down_revision = ('5d8d48f2c1ed', '047f07692963')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
